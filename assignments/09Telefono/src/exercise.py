067ed1f6b7a49236c70f71b70a36372ba3d39006
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-18 15:13:51 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,22 @@
 def main():
     #escribe tu código abajo de esta línea
     #Leer los datos
+    # Elia Melissa Lugo Vences
+    # A01660980
+    # Costo del teléfono
+
+    print ("Dame el número de mensajes: ")
+    msj=int(input())
+    print ("Dame el número de megas: ")
+    megas=float(input())
+    print ("Dame el número de minutos ")
+    minutos=int(input())
+
+    costo = (msj*0.80) + (megas*0.80) + (minutos*0.80)
+
+    print ("El costo mensual es: ")
+    print (costo)
+
     pass
 
 
