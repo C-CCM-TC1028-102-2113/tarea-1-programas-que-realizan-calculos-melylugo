953a009c6e604da46b4765ee4758e13310faa3d4
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-18 17:47:30 -0500

    Videojuegos

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,8 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
+    # Elia Melissa Lugo Vences
+    # A01660980
+    # Videojuegos
+
+    print ("Dame la cantidad de juegos nuevos: ")
+    new=int(input())
+    print ("Dame la cantidad de juegos usados: ")
+    old=int(input())
+
+    total=(new*1000)+(old*350)
+
+    print ("El total de la compra es: ")
+    print (total) 
+
    pass
-
-
 
 if __name__ == '__main__':
     main()
