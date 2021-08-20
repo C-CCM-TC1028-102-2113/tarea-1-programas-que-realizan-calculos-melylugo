f980b81a481674eebbade8be7fef968cef06b9ee
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-20 14:55:20 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,9 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea   
-    minn=float(input("Dame los minutos: "))
-    x=(5.7*6)*minn
-    decimales=round(x,1)
-    print ("Centímentros recorridos: ", decimales)
     
+    minutos=float(input("Dame los minutos: "))
+
+    distancia=5.7*6*minutos
+    distancia=round(distancia,1)
+
+    print ("Centímentros recorridos: ", distancia)
+    pass
+
 if __name__ == '__main__':
     main()
