ab434b6383045c948f0d3be191e6f591f1df6692
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-20 15:05:34 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -3,8 +3,10 @@ def main():
     msj=int(input("Dame el número de mensajes: "))
     megas=float(input("Dame el número de megas: "))
     minutos=int(input("Dame el número de minutos "))
+    
     costo = (msj*0.80) + (megas*0.80) + (minutos*0.80)
     costo=round(costo,2)
+    
     print ("El costo mensual es: ", costo)
     pass
 
