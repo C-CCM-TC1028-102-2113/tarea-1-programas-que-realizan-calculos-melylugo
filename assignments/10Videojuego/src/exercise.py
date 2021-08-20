4866810b69464f383b423766140f3a86d7a0a675
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-20 15:02:36 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,17 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    
-    # Elia Melissa Lugo Vences
-    # A01660980
-    # Videojuegos
-
     new=int(input("Dame la cantidad de juegos nuevos: "))
     old=int(input("Dame la cantidad de juegos usados: "))
-
-    total=(new*1000)+(old*350)
-
+    total=int((new*1000)+(old*350))
     print ("El total de la compra es: ", total)
-    
     pass
 
 if __name__ == '__main__':
