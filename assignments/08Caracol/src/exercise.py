23668aa374a507eed45d4aef46797c39dcc9ecdd
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-20 15:05:11 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,8 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea   
     minutos=float(input("Dame los minutos: "))
+    
     distancia=(5.7*6)*minutos
     distancia=round(distancia,1)
+    
     print ("Centímentros recorridos: ", distancia)
     pass
 
