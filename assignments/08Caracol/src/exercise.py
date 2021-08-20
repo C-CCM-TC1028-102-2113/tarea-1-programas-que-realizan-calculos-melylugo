570ed3407827940135fb66578ff64c10431ad192
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-20 13:41:51 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,15 +1,9 @@
 def main():
-    #escribe tu código abajo de esta línea
-    # Elia Melissa Lugo Vences
-    # A01660980
-    # Programa que calcula la distancia recorrida por un caracol"
-    
+    #escribe tu código abajo de esta línea   
     minutos=float(input("Dame los minutos: "))
-
     distancia=(5.7*6)*minutos
     distancia=round(distancia,1)
-    
     print ("Centímentros recorridos: ", distancia)
-
+    
 if __name__ == '__main__':
     main()
