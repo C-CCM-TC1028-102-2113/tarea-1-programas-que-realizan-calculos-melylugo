f1ac518ef7a7f3af10b84c260077e0a6890891d0
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-20 15:06:20 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    import math
 
+    palabras=int(input("Dame el numero de palabras: "))
+
+    x=(math.ceil(palabras/475))*60
+    desc=x*0.10
+    Total=x-desc
+    Total=round(Total,1)
+
+    print(Total)
+    pass
 
 if __name__ == '__main__':
     main()
