1203a23d9414f800c26ca369e2d3efecf9e82b78
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-21 16:51:52 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -2,14 +2,14 @@ def main():
     #escribe tu código abajo de esta línea
     import math
 
-    palabras=int(input("Dame el numero de palabras: "))
+    palabras=int(input("Dame el número de palabras: "))
 
     x=(math.ceil(palabras/475))*60
     desc=x*0.10
     Total=x-desc
     Total=round(Total,1)
 
-    print(Total)
+    print("El costo de la publicación es:",Total)
     pass
 
 if __name__ == '__main__':
